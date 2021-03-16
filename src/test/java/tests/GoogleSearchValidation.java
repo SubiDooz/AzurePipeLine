b2fbf1da8ae2ee9d0f9eb4a85566c1bd7bb76807
SubiDooz <geeksubhra@gmail.com>
2021-03-16 19:23:27 +0530

    updated testclass file name

---
+++ src/test/java/tests/GoogleSearchValidation.java
@@ -0,0 +1,75 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import pages.GoogleSearchPage;
+
+
+public class GoogleSearchValidation {
+	
+	WebDriver driver;
+	
+	@Test
+	public void validatePage() {
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		options.addArguments("disable-infobars");
+		options.addArguments("--disable-extensions");
+		//options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-gpu");
+		//options.addArguments("--headless");
+		
+		
+		driver = new ChromeDriver(options);
+		
+		//driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		
+		
+		GoogleSearchPage gspObj = new GoogleSearchPage(driver);
+		gspObj.enterValInSearchBox("Azure");
+		driver.close();
+		Assert.assertTrue(true);
+		
+	}
+	
+	@Test
+	public void failTestCase() {
+		WebDriverManager.chromedriver().setup();
+		
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized");
+		options.addArguments("disable-infobars");
+		options.addArguments("--disable-extensions");
+		//options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--disable-extensions");
+		options.addArguments("--no-sandbox");
+		options.addArguments("--disable-gpu");
+		//options.addArguments("--headless");
+		
+		
+		driver = new ChromeDriver(options);
+		
+		//driver.manage().window().maximize();
+		driver.get("https://www.google.com");
+		
+		
+		GoogleSearchPage gspObj = new GoogleSearchPage(driver);
+		gspObj.enterValInSearchBox("Azure");
+		driver.close();
+		Assert.assertTrue(false);
+		
+	}
+	
+	
+	
+}
